c2b3df4c2445c3134fec77728f9bb379782218e9
Achmaddevridarmawan <39895658+Achmaddevridarmawan@users.noreply.github.com>
2018-07-08 23:14:27 +0700

    login + logout
    Menambahkan login, menambahkan logout

--- application/views/header.php
+++ application/views/header.php
@@ -24,6 +24,8 @@
     <link rel="stylesheet" href="<?php echo base_url('template_admin/') ?>assets/scss/style.css">
     <link href="<?php echo base_url('template_admin/') ?>assets/css/lib/vector-map/jqvmap.min.css" rel="stylesheet">
 
+    
+
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
 
     <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> -->
@@ -37,7 +39,7 @@
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
                     <i class="fa fa-bars"></i>
                 </button>
-                <a class="navbar-brand" href="<?php echo base_url('template_admin/') ?>./"><img src="<?php echo base_url('template_admin/') ?>images/logo.png" alt="Logo"></a>
+                <a class="navbar-brand" href="<?php echo base_url('') ?>Admin"><img src="<?php echo base_url('template_admin/') ?>images/logo.png" alt="Logo"></a>
                 <a class="navbar-brand hidden" href="./"><img src="<?php echo base_url('template_admin/') ?>images/logo2.png" alt="Logo"></a>
             </div>
 
@@ -92,8 +94,8 @@
                           <button class="btn btn-secondary dropdown-toggle" type="button" id="notification" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <i class="fa fa-bell"></i>
                             <span class="count bg-danger">5</span>
-                          </button>
-                          <div class="dropdown-menu" aria-labelledby="notification">
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="notification">
                             <p class="red">You have 3 Notification</p>
                             <a class="dropdown-item media bg-flat-color-1" href="#">
                                 <i class="fa fa-check"></i>
@@ -107,94 +109,94 @@
                                 <i class="fa fa-warning"></i>
                                 <p>Server #3 overloaded.</p>
                             </a>
-                          </div>
-                        </div>
-
-                        <div class="dropdown for-message">
-                          <button class="btn btn-secondary dropdown-toggle" type="button"
-                                id="message"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="ti-email"></i>
-                            <span class="count bg-primary">9</span>
-                          </button>
-                          <div class="dropdown-menu" aria-labelledby="message">
-                            <p class="red">You have 4 Mails</p>
-                            <a class="dropdown-item media bg-flat-color-1" href="#">
-                                <span class="photo media-left"><img alt="avatar" src="<?php echo base_url('template_admin/') ?>images/avatar/1.jpg"></span>
-                                <span class="message media-body">
-                                    <span class="name float-left">Jonathan Smith</span>
-                                    <span class="time float-right">Just now</span>
-                                        <p>Hello, this is an example msg</p>
-                                </span>
-                            </a>
-                            <a class="dropdown-item media bg-flat-color-4" href="#">
-                                <span class="photo media-left"><img alt="avatar" src="<?php echo base_url('template_admin/') ?>images/avatar/2.jpg"></span>
-                                <span class="message media-body">
-                                    <span class="name float-left">Jack Sanders</span>
-                                    <span class="time float-right">5 minutes ago</span>
-                                        <p>Lorem ipsum dolor sit amet, consectetur</p>
-                                </span>
-                            </a>
-                            <a class="dropdown-item media bg-flat-color-5" href="#">
-                                <span class="photo media-left"><img alt="avatar" src="<?php echo base_url('template_admin/') ?>images/avatar/3.jpg"></span>
-                                <span class="message media-body">
-                                    <span class="name float-left">Cheryl Wheeler</span>
-                                    <span class="time float-right">10 minutes ago</span>
-                                        <p>Hello, this is an example msg</p>
-                                </span>
-                            </a>
-                            <a class="dropdown-item media bg-flat-color-3" href="#">
-                                <span class="photo media-left"><img alt="avatar" src="<?php echo base_url('template_admin/') ?>images/avatar/4.jpg"></span>
-                                <span class="message media-body">
-                                    <span class="name float-left">Rachel Santos</span>
-                                    <span class="time float-right">15 minutes ago</span>
-                                        <p>Lorem ipsum dolor sit amet, consectetur</p>
-                                </span>
-                            </a>
-                          </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-sm-5">
-                    <div class="user-area dropdown float-right">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <img class="user-avatar rounded-circle" src="<?php echo base_url('template_admin/') ?>images/admin.jpg" alt="User Avatar">
-                        </a>
-
-                        <div class="user-menu dropdown-menu">
-                                <a class="nav-link" href="#"><i class="fa fa- user"></i>My Profile</a>
-
-                                <a class="nav-link" href="#"><i class="fa fa- user"></i>Notifications <span class="count">13</span></a>
-
-                                <a class="nav-link" href="#"><i class="fa fa -cog"></i>Settings</a>
-
-                                <a class="nav-link" href="#"><i class="fa fa-power -off"></i>Logout</a>
                         </div>
                     </div>
 
-                    <div class="language-select dropdown" id="language-select">
-                        <a class="dropdown-toggle" href="#" data-toggle="dropdown"  id="language" aria-haspopup="true" aria-expanded="true">
-                            <i class="flag-icon flag-icon-us"></i>
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="language" >
-                            <div class="dropdown-item">
-                                <span class="flag-icon flag-icon-fr"></span>
-                            </div>
-                            <div class="dropdown-item">
-                                <i class="flag-icon flag-icon-es"></i>
-                            </div>
-                            <div class="dropdown-item">
-                                <i class="flag-icon flag-icon-us"></i>
-                            </div>
-                            <div class="dropdown-item">
-                                <i class="flag-icon flag-icon-it"></i>
-                            </div>
-                        </div>
-                    </div>
-
+                    <div class="dropdown for-message">
+                      <button class="btn btn-secondary dropdown-toggle" type="button"
+                      id="message"
+                      data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <i class="ti-email"></i>
+                      <span class="count bg-primary">9</span>
+                  </button>
+                  <div class="dropdown-menu" aria-labelledby="message">
+                    <p class="red">You have 4 Mails</p>
+                    <a class="dropdown-item media bg-flat-color-1" href="#">
+                        <span class="photo media-left"><img alt="avatar" src="<?php echo base_url('template_admin/') ?>images/avatar/1.jpg"></span>
+                        <span class="message media-body">
+                            <span class="name float-left">Jonathan Smith</span>
+                            <span class="time float-right">Just now</span>
+                            <p>Hello, this is an example msg</p>
+                        </span>
+                    </a>
+                    <a class="dropdown-item media bg-flat-color-4" href="#">
+                        <span class="photo media-left"><img alt="avatar" src="<?php echo base_url('template_admin/') ?>images/avatar/2.jpg"></span>
+                        <span class="message media-body">
+                            <span class="name float-left">Jack Sanders</span>
+                            <span class="time float-right">5 minutes ago</span>
+                            <p>Lorem ipsum dolor sit amet, consectetur</p>
+                        </span>
+                    </a>
+                    <a class="dropdown-item media bg-flat-color-5" href="#">
+                        <span class="photo media-left"><img alt="avatar" src="<?php echo base_url('template_admin/') ?>images/avatar/3.jpg"></span>
+                        <span class="message media-body">
+                            <span class="name float-left">Cheryl Wheeler</span>
+                            <span class="time float-right">10 minutes ago</span>
+                            <p>Hello, this is an example msg</p>
+                        </span>
+                    </a>
+                    <a class="dropdown-item media bg-flat-color-3" href="#">
+                        <span class="photo media-left"><img alt="avatar" src="<?php echo base_url('template_admin/') ?>images/avatar/4.jpg"></span>
+                        <span class="message media-body">
+                            <span class="name float-left">Rachel Santos</span>
+                            <span class="time float-right">15 minutes ago</span>
+                            <p>Lorem ipsum dolor sit amet, consectetur</p>
+                        </span>
+                    </a>
                 </div>
             </div>
+        </div>
+    </div>
 
-        </header><!-- /header -->
+    <div class="col-sm-5">
+        <div class="user-area dropdown float-right">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <img class="user-avatar rounded-circle" src="<?php echo base_url('template_admin/') ?>images/admin.jpg" alt="User Avatar">
+            </a>
+
+            <div class="user-menu dropdown-menu">
+                <a class="nav-link" href="#"><i class="fa fa- user"></i><?php echo $this->session->userdata('logged_in')['nama']; ?></a>
+
+                <a class="nav-link" href="#"><i class="fa fa- user"></i>Notifications <span class="count">13</span></a>
+
+                <a class="nav-link" href="#"><i class="fa fa -cog"></i>Settings</a>
+
+                <a class="nav-link" href="<?php echo base_url('Login/logout') ?>"><i class="fa fa-power -off"></i>Logout</a>
+            </div>
+        </div>
+
+        <div class="language-select dropdown" id="language-select">
+            <a class="dropdown-toggle" href="#" data-toggle="dropdown"  id="language" aria-haspopup="true" aria-expanded="true">
+                <i class="flag-icon flag-icon-us"></i>
+            </a>
+            <div class="dropdown-menu" aria-labelledby="language" >
+                <div class="dropdown-item">
+                    <span class="flag-icon flag-icon-fr"></span>
+                </div>
+                <div class="dropdown-item">
+                    <i class="flag-icon flag-icon-es"></i>
+                </div>
+                <div class="dropdown-item">
+                    <i class="flag-icon flag-icon-us"></i>
+                </div>
+                <div class="dropdown-item">
+                    <i class="flag-icon flag-icon-it"></i>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+</header><!-- /header -->
         <!-- Header-->
